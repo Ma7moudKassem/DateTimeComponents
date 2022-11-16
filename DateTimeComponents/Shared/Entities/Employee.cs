c39473d5fd585d1f114928ca62914bef01f00044
Ma7moud Kassem <Ma7moud Kassem@DESKTOP-9IVLO3P>
2022-11-16 11:42:19 +0200

    commit

--- DateTimeComponents/Shared/Entities/Employee.cs
+++ DateTimeComponents/Shared/Entities/Employee.cs
@@ -7,5 +7,8 @@ public class Employee
     public string Address { get; set; }
     public int Age { get; set; }
     public TimeSpan Time { get; set; }
+    public TimeSpan SecondTime { get; set; }
+    public TimeSpan ThirdTime { get; set; }
     public DateTime DateTime { get; set; }
+    public DateTime ThirdDateTime { get; set; }
 }
